8edbf6d650b220cbda2e84473686c75445beab61
Wahid Fayeq <wahid.fayaq@yahoo.com>
2026-07-11 13:28:11 -0400

    Add notes field to job model

--- server/models/Job.js
+++ server/models/Job.js
@@ -26,6 +26,11 @@ const jobSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    notes: {
+      type: String,
+      trim: true,
+      default: "",
+    },
   },
   {
     timestamps: true,
